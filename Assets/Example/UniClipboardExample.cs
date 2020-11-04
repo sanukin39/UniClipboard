bc982e1a636b233a339476c4f117efa5d37df71a
sanukin39 <ericsson.goodjob@gmail.com>
2020-11-04 11:59:37 +0900

    AddExampleScene

---
+++ Assets/Example/UniClipboardExample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UniClipboardExample : MonoBehaviour
+{
+    [SerializeField] private Button _getTextButton = null;
+    [SerializeField] private Button _setTextButton = null;
+    [SerializeField] private InputField _setTextInputField = null;
+    [SerializeField] private Text _getText = null;
+
+    private void Awake()
+    {
+        _getTextButton.onClick.AddListener(GetText);
+        _setTextButton.onClick.AddListener(SetText);
+    }
+
+    void GetText()
+    {
+        _getText.text = UniClipboard.GetText();
+    }
+
+    void SetText()
+    {
+        var text = _setTextInputField.text;
+        UniClipboard.SetText(text);
+    }
+}
+
+
